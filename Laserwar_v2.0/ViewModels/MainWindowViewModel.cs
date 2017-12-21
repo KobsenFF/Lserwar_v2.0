67ec7210ccf72b7ce89819d2dad8b9f94c8defe5
Konstantin Vasiliev <vasilevkns@rambler.ru>
2017-12-22 01:03:15 +0300

    Add Resources\Images directory, added downloadView

--- Laserwar_v2.0/ViewModels/MainWindowViewModel.cs
+++ Laserwar_v2.0/ViewModels/MainWindowViewModel.cs
@@ -1,12 +1,15 @@
 ﻿namespace Laserwar_v2._0.ViewModels
 {
+    using Catel.Data;
     using Catel.MVVM;
     using System.Threading.Tasks;
+    using System.Windows;
 
     public class MainWindowViewModel : ViewModelBase
     {
         public MainWindowViewModel()
         {
+            Close_ClickCommand = new Command(OnClose_ClickCommandExecute);
         }
 
         public override string Title { get { return "Laserwar_v2._0"; } }
@@ -14,19 +17,23 @@
         // TODO: Register models with the vmpropmodel codesnippet
         // TODO: Register view model properties with the vmprop or vmpropviewmodeltomodel codesnippets
         // TODO: Register commands with the vmcommand or vmcommandwithcanexecute codesnippets
-
+       
         protected override async Task InitializeAsync()
         {
             await base.InitializeAsync();
-
-            // TODO: subscribe to events here
         }
 
         protected override async Task CloseAsync()
         {
-            // TODO: unsubscribe from events here
-
             await base.CloseAsync();
+        }
+
+
+        public Command Close_ClickCommand { get; private set; }
+
+        private void OnClose_ClickCommandExecute()
+        {
+            Application.Current.Shutdown();
         }
     }
 }
---
+++ Laserwar_v2.0/Views/DownloadView.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Laserwar_v2._0.Views
+{
+    public partial class DownloadView
+    {
+        public DownloadView()
+        {
+            InitializeComponent();
+        }
+    }
+}
